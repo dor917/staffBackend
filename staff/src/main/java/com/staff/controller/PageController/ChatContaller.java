8e0ba60508da96611ab27ab08e2c508d515db3be
nahoyeon <nahy0107@naver.com>
2022-06-05 16:26:24 +0900

    Revert "채팅"
    This reverts commit 415c9c5d5ee81ba86168e9a9306eb065aaa17d1b.

--- staff/src/main/java/com/staff/controller/PageController/ChatContaller.java
+++ staff/src/main/java/com/staff/controller/PageController/ChatContaller.java
@@ -1,12 +1,20 @@
 package com.staff.controller.PageController;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
+import java.util.Vector;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,14 +29,22 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.fasterxml.jackson.core.JsonParser;
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelSftp;
 import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+import com.staff.model.ChatListVo;
+import com.staff.model.ChatVO;
 import com.staff.util.ListeningThread;
 import com.staff.util.SFTPUtil;
 import com.staff.util.WritingThread;
 
 @RestController
 public class ChatContaller {
-	
+	final static String ID = "staff";
+	final static String IP = "3.36.120.248";
+	final static int SSHPORT = 22;
+	final static String PW = "staff123";
 	Socket socket = null;
 	@RequestMapping("/sendChat.staff")
 	public String chatHistory(HttpServletRequest req, HttpServletResponse res) throws Exception {
@@ -50,7 +66,97 @@ public class ChatContaller {
 		
 		
 	}
-	public void connChatServer(String ip, int port, String message) {
+
+	@RequestMapping("/getChatHistory.staff")
+	public ArrayList<ChatListVo> getChatHistory(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		 res.setHeader("Access-Control-Allow-Origin", "*"); //허용대상 도메인
+		ArrayList<ChatListVo> result = new ArrayList<>();
+
+		Session session = null;
+		JSch jSch = null;
+		ArrayList<String> fileList = new ArrayList<>();
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMdd"); 
+		SimpleDateFormat changeSimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd"); 
+		Date chatDate;
+		String readStr = "";
+		String dateStr;
+		try {
+			// 1. ssh 연결 설정 
+			String prj_no = req.getParameter("prj_no");
+			jSch = new JSch();
+			session = jSch.getSession(ID, IP, SSHPORT);
+			session.setPassword(PW);
+			
+			Properties properties = new Properties();
+			properties.put("StrictHostKeyChecking", "no");
+			session.setConfig(properties);
+			
+			// 2. ssh 연결
+			session.connect();
+			
+			Channel channel = session.openChannel("sftp");
+			ChannelSftp channelSftp = (ChannelSftp) channel;
+			channelSftp.connect();
+			
+			//3. 파일 읽기
+			StringBuffer stringBuffer = new StringBuffer();
+			String dir = "/staffChat/"+prj_no;
+			channelSftp.cd(dir);
+			
+			Vector<ChannelSftp.LsEntry> files = channelSftp.ls(dir);
+			
+			for(ChannelSftp.LsEntry entry : files){
+				if(!entry.getFilename().equals(".") && !entry.getFilename().equals("..")  && entry.getFilename().indexOf(".") != 0){
+					fileList.add(entry.getFilename());
+		         }
+			}
+			Collections.sort(fileList);
+			// 4. return 설정
+			
+			if(fileList.size() > 0) {
+				Date date = simpleDateFormat.parse(fileList.get(0));
+				dateStr = changeSimpleDateFormat.format(date);
+				
+				for (String  file : fileList) {
+					if (!dateStr.equals(file)) {
+						date = simpleDateFormat.parse(file);
+						dateStr = changeSimpleDateFormat.format(date);
+					}
+					ChatListVo chatListVo = new ChatListVo();
+					ArrayList<ChatVO> reListList  = new ArrayList<>();
+					BufferedReader reader = new BufferedReader(new InputStreamReader(channelSftp.get(file)));
+					while((readStr = reader.readLine()) != null && !"".equals(readStr)) {
+						String[] chatArr = readStr.split("!@##@!");
+						ChatVO chatVo = new ChatVO();
+						chatVo.setPrj_no(prj_no);
+						chatVo.setMbr_email(chatArr[0]);
+						chatVo.setMessage(chatArr[1]);
+						reListList.add(chatVo);
+					}
+					chatListVo.setDate(dateStr);
+					chatListVo.setChatList(reListList);
+					result.add(chatListVo);
+					
+				}
+				
+			}
+
+			
+			
+			
+			
+			
+			
+
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+
+	public static void connChatServer(String ip, int port, String message) {
 		Socket socket = null;
 		PrintWriter pw = null;
 		try {
---
+++ staff/src/main/java/com/staff/model/ChatVO.java
@@ -0,0 +1,12 @@
+package com.staff.model;
+
+import lombok.Data;
+
+@Data
+public class ChatVO {
+
+	private String prj_no;
+	private String mbr_email;
+	private String message;
+	
+}
